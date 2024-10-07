9768201b66c64913ea74f5ec932d4abf727c7869
rendzina <s.hallett@cranfield.ac.uk>
2024-10-07 22:04:39 +0100

    added scripts

---
+++ scripts/driverscript.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+################################################################################
+# Name         : installsql_php.sh
+# Description  : This script installs the Microsoft ODBC driver 18 from the bash shell for Ubuntu and was copied 
+#                directly from the Microsoft learn/SQL/SQL server web page
+#
+# Reference    : https://learn.microsoft.com/en-us/sql/connect/odbc/linux-mac/installing-the-microsoft-odbc-driver-for-sql-server?view=azuresqldb-current
+# Requires     : PHP 8.3
+#
+# Course       : cloud computing MK5014/MK6014
+# Date         : 04/10/2024
+# Version      : 1.0
+################################################################################
+
+# Check version of Ubuntu is 18.04 or later
+if ! [[ "18.04 20.04 22.04 23.04 24.04" == *"$(lsb_release -rs)"* ]];
+then
+    echo "Ubuntu $(lsb_release -rs) is not currently supported.";
+    exit;
+fi
+
+# Add the signature to trust the Microsoft repo
+# For Ubuntu versions < 24.04 
+# curl https://packages.microsoft.com/keys/microsoft.asc | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc
+# For Ubuntu versions >= 24.04
+curl https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor -o /usr/share/keyrings/microsoft-prod.gpg
+
+# Add repo to apt sources
+curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list | sudo tee /etc/apt/sources.list.d/mssql-release.list
+
+# Install the driver
+sudo apt-get update
+sudo ACCEPT_EULA=Y apt-get install -y msodbcsql18
+# optional: for bcp and sqlcmd
+sudo ACCEPT_EULA=Y apt-get install -y mssql-tools18
+echo 'export PATH="$PATH:/opt/mssql-tools18/bin"' >> ~/.bashrc
+source ~/.bashrc
+# optional: for unixODBC development headers
+sudo apt-get install -y unixodbc-dev
---
+++ scripts/install_php.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+################################################################################
+# Name         : installphp.sh
+# Description  : This script installs php and the php Apache2 package
+#
+# Reference    : https://documentation.ubuntu.com/server/how-to/web-services/install-php/
+# Requires     : Ubuntu 24.04, Apache2
+#
+# Course       : cloud computing MK5014/MK6014
+# Date         : 04/10/2024
+# Version      : 1.0
+################################################################################
+
+# check version of ubuntu - exit if not latest
+if ! [[ "24.04" == *"$(lsb_release -rs)"* ]];
+then
+  echo "Ubuntu $(lsb_release -rs) is not currently supported.";
+  exit;
+else
+ echo "Ubuntu $(lsb_release -rs)"
+fi
+
+# check Apache is installed. This is required before we install php. exit if not
+if [[ -z $(apache2 -v 2>/dev/null) ]] && [[ -z $(httpd -v 2>/dev/null) ]]; 
+then 
+   echo "Apache not found"; 
+   exit;
+else
+  echo "Apache installed"; 
+fi
+
+# install PHP and libapache2-mod-php package
+# libapache2_mod_php is the PHP plugin in Apache
+echo "installing php"
+sudo apt install php libapache2-mod-php
+
+# restart Apache2 web server
+echo "restarting apache2 service"
+sudo systemctl restart apache2.service
+
+# show php version-  must be PHP 8.3
+echo "Current version of PHP:"
+php -version
